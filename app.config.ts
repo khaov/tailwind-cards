d2a78400795a9b8f6dfbf23538efcb4bf33a94b2
Artyon Khaov <a.khaov@gmail.com>
2024-11-27 17:31:26 +0700

    feat: added contacts cards basic layout

---
+++ app.config.ts
@@ -0,0 +1,12 @@
+export default defineAppConfig({
+    ui: {
+      button: {
+        color: {
+            white: {
+              outline: 'text-white ring-1 ring-inset ring-current'
+            }
+        }
+      }
+    }
+  })
+  
--- app.vue
+++ app.vue
@@ -1,10 +1,7 @@
-<script setup lang="ts">
-import type { Link } from '#build/components';
-import { _disabled } from '#tailwind-config/theme/aria';
-</script>
-
 <template>
-  <div>
+  <section>
+    <h2>Вера Михайловна, вам оказаны услуги</h2>
+
     <ul class="grid gap-4 md:grid-cols-2 md:gap-5 lg:grid-cols-3">
       <li
         v-for="(item, index) in [
@@ -33,10 +30,44 @@ import { _disabled } from '#tailwind-config/theme/aria';
           <p class="text-sm leading-5 font-normal text-slate-300">{{ item.description }}</p>
 
           <UButton v-if="item.link" class="text-emerald-400" size="lg" variant="link" icon="heroicons:arrow-down-solid" trailing>Ознакомиться</UButton>
-          <UButton v-else size="lg"  variant="solid" disabled>Рейтинг заёмщика</UButton>
+          <UButton v-else size="lg" variant="solid" disabled>Рейтинг заёмщика</UButton>
 
         </div>
       </li>
     </ul>
-  </div>
+  </section>
+
+  <section>
+    <ul class="grid gap-4 md:grid-cols-2 md:gap-5 lg:grid-cols-3">
+      <li
+        v-for="(support, index) in [
+          {
+            icon:   '',
+            title:  'Помощь VK',
+          },
+
+          {
+            icon:   '',
+            title:  'Бот в телеграм',
+          },
+
+          {
+            icon:   'i-heroicons-rocket-launch-solid',
+            title:  'Помощь тех.поддержки',
+          },
+        ]"
+        :key="index"
+      >
+        <div class="grid gap-y-4 h-full px-8 py-6 rounded-[32px] bg-[#1F1F1F]">
+
+          <div class="flex gap-x-4">
+            <UIcon name='' class="w-8 h-8 text-white" />
+            <h3 class="text-xl leading-6 font-medium text-white">{{ support.title }}</h3>
+          </div>
+
+          <UButton class="text-center" size="lg" color="white" variant="outline">Написать</UButton>
+        </div>
+      </li>
+    </ul>
+  </section>
 </template>
